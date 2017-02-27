dd2bccb1886fcaa3faf2e78865cabfb5068d132d
zy <zeng.yu.1993@gmail.com>
2017-02-27 23:37:55 +0800

    + 实现了 Config.basic_configer 中关于文件读写和配置数据增改的方法。

--- api/Config.py
+++ api/Config.py
@@ -12,22 +12,37 @@ class basic_configer(object):
         except TypeError:
             pass
 
+        self._configer = self._open_config_file()
         self._section = self._define_secton()
-        self.__dict__.update(self._get_options())
+        self._options = self._get_options()
+        self.__dict__.update(self._options)
 
     def get(self, option, default=None):
-        raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+        return self.__dict__.get(option, default)
 
     def set(self, option, value):
-        raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+        if not hasattr(self, option):
+            raise NoOptionError(self._section + 'has no option: ' + option)
+        self.add(option, value)
 
     def add(self, option, value):
-        raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+        self.__dict__[option] = value
+        self._configer.set(self._section, option, value)
+        self._write_config_file()
 
     def _define_secton(self):
         raise NotImplemented(__name__ + self.__class__ + 'not implemented')
 
     def _get_options(self):
+        return {name: value for (name, value) in self._configer.items(self._section)}
+
+    def _open_config_file(self):
+        fp = open(self.config_file_path, 'r')  # 如果找不到配置文件，将会抛出 IOError
+        fp.close()
         configer = ConfigParser()
         configer.read(self.config_file_path)
-        return {name: value for (name, value) in configer.items(self._define_secton())}
+        return configer
+
+    def _write_config_file(self):
+        with open(self.config_file_path, 'w') as fp:
+            self._configer.write(fp)
