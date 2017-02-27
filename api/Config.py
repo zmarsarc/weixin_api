e05bce508f106a1bff988fb827de35edcc020dc8
zy <zeng.yu.1993@gmail.com>
2017-02-27 23:08:08 +0800

    + 实现了 Config.basic_configer 中定义 section name 和为对象动态添加属性的方法。

--- api/Config.py
+++ api/Config.py
@@ -7,7 +7,13 @@ class basic_configer(object):
     config_file_path = 'config.cfg'
 
     def __int__(self):
-        super(basic_configer, self).__int__()
+        try:
+            super(basic_configer, self).__int__()
+        except TypeError:
+            pass
+
+        self._section = self._define_secton()
+        self.__dict__.update(self._get_options())
 
     def get(self, option, default=None):
         raise NotImplemented(__name__ + self.__class__ + 'not implemented')
@@ -17,3 +23,11 @@ class basic_configer(object):
 
     def add(self, option, value):
         raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+
+    def _define_secton(self):
+        raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+
+    def _get_options(self):
+        configer = ConfigParser()
+        configer.read(self.config_file_path)
+        return {name: value for (name, value) in configer.items(self._define_secton())}
