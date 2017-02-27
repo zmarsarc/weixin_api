42fb792c9c0e83104b69e731014594f50ae87e49
zy <zeng.yu.1993@gmail.com>
2017-02-27 16:34:30 +0800

    + 实现 token.refresh token._get_token token._get_config。

--- api/Access.py
+++ api/Access.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from ConfigParser import ConfigParser
+import requests
+import json
+import time
 
 
 class token(object):
@@ -8,13 +12,29 @@ class token(object):
         self.update = None
         self.ttl = None
 
-        self._config = None  # 保存获取 token 所需的配置信息，并且将 token 回写。
+        self._config = self._get_config()  # 保存获取 token 所需的配置信息，并且将 token 回写。
 
     def refresh(self):
-        pass
+        self.value, self.ttl = self._get_token()
+        self.update = int(time.time())
+        self._set_config()
 
     def _get_token(self):
-        pass
+        params = {'grant_type': 'client_credential',
+                  'appid': self._config.appid,
+                  'secret': self._config.secret}
+        response = requests.get(self._config.token_api, params=params)
+        response = json.loads(response.content)
+        return (response['access_token'], response['expires_in'])
 
     def _get_config(self):
-        pass
+        configer = ConfigParser()
+        configer.read('config.cfg')
+        ret = object()
+        ret.token_api = 'https://api.weixin.qq.com/cgi-bin/token'
+        ret.appid = configer.get('App_info', 'id')
+        ret.secret = configer.get('App_info', 'secret')
+        return ret
+
+    def _set_config(self):
+        raise NotImplemented('Access.token._set_config not implemented')
