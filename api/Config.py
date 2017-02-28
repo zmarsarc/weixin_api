e7c42f8f29ee7b905d52d6a916890673cd5b731a
zy <zeng.yu.1993@gmail.com>
2017-02-28 21:08:20 +0800

    * 修正了 Config.basic_configer 中的多处错误。

--- api/Config.py
+++ api/Config.py
@@ -4,25 +4,26 @@ from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 
 class basic_configer(object):
 
-    config_file_path = 'config.cfg'
-
-    def __int__(self):
+    def __init__(self, filename):
         try:
-            super(basic_configer, self).__int__()
+            super(basic_configer, self).__init__(filename)
         except TypeError:
-            pass
+            super(basic_configer, self).__init__()
 
+        self.config_file = filename
         self._configer = self._open_config_file()
-        self._section = self._define_secton()
+        self._section = self._define_section()
         self._options = self._get_options()
+        self.__class__.__slots__ = [name for name in self._options.keys()]
         self.__dict__.update(self._options)
+        self._limited = True
 
     def get(self, option, default=None):
         return self.__dict__.get(option, default)
 
     def set(self, option, value):
         if not hasattr(self, option):
-            raise NoOptionError(self._section + 'has no option: ' + option)
+            raise NoOptionError(self._section, option)
         self.add(option, value)
 
     def add(self, option, value):
@@ -30,19 +31,35 @@ class basic_configer(object):
         self._configer.set(self._section, option, value)
         self._write_config_file()
 
-    def _define_secton(self):
-        raise NotImplemented(__name__ + self.__class__ + 'not implemented')
+    def _define_section(self):
+        raise NotImplementedError(__name__ + str(self.__class__) + 'not implemented')
+
+    def _define_slots(self):
+        raise NotImplementedError(__name__ + str(self.__class__) + ' _define_slots not implemented')
 
     def _get_options(self):
         return {name: value for (name, value) in self._configer.items(self._section)}
 
     def _open_config_file(self):
-        fp = open(self.config_file_path, 'r')  # 如果找不到配置文件，将会抛出 IOError
+        fp = open(self.config_file, 'r')  # 如果找不到配置文件，将会抛出 IOError
         fp.close()
         configer = ConfigParser()
-        configer.read(self.config_file_path)
+        configer.read(self.config_file)
         return configer
 
     def _write_config_file(self):
-        with open(self.config_file_path, 'w') as fp:
+        with open(self.config_file, 'w') as fp:
             self._configer.write(fp)
+
+    def __setattr__(self, key, value):
+        if not hasattr(self, '_limited'):
+            self.__dict__['_limited'] = False
+        if self._limited and key not in self.__dict__.keys():
+            raise AttributeError('no such option ' + key)
+        self.__dict__[key] = value
+
+
+class app(basic_configer):
+
+    def _define_section(self):
+        return 'App'
