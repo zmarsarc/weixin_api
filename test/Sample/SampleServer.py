f2bbd128e39bc9faf425933f82c2ef45ace29029
zy <zeng.yu.1993@gmail.com>
2017-03-03 15:51:45 +0800

    + 为测试用服务器增加了参数检查。

--- test/Sample/SampleServer.py
+++ test/Sample/SampleServer.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request
+from flask import Flask, request, abort
 import json
 
+config = {
+    'id': 'zmarsarc',
+    'secret': '123456',
+    'type': 'client_credential'
+}
 
 app = Flask('FakeServer')
 
 
 @app.route('/cgi-bin/token')
 def token():
-    if request.method == 'GET':
+    if request.method == 'GET' and is_params_valid(request.args):
         response = {
             'access_token': 'ACCESS_TOKEN',
             'expires_in': 7200
         }
         return json.JSONEncoder().encode(response)
+    return abort(500)
+
+
+def is_params_valid(params):
+    try:
+        return (params['grant_type'] == config['type'] and
+                params['appid'] == config['id'] and
+                params['secret'] == config['secret'])
+    except KeyError:
+        return False
 
 app.run(port=12345)
