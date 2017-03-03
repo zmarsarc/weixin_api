810de35e8c47dd9572a8518ba32eb3ce7f7eb8fa
zy <zeng.yu.1993@gmail.com>
2017-03-03 15:20:10 +0800

    - 将 Access 所需的配置数据改为使用参数传递。

--- api/Access.py
+++ api/Access.py
@@ -7,12 +7,12 @@ import time
 
 class token(object):
 
-    def __init__(self):
+    def __init__(self, config):
         self.value = None
         self.update = None
         self.ttl = None
 
-        self._config = self._get_config()  # 保存获取 token 所需的配置信息，并且将 token 回写。
+        self._config = config  # 保存获取 token 所需的配置信息，并且将 token 回写。
 
     def refresh(self):
         self.value, self.ttl = self._get_token()
@@ -26,15 +26,6 @@ class token(object):
         response = requests.get(self._config.token_api, params=params)
         response = json.loads(response.content)
         return (response['access_token'], response['expires_in'])
-
-    def _get_config(self):
-        configer = ConfigParser()
-        configer.read('config.cfg')
-        ret = object()
-        ret.token_api = 'https://api.weixin.qq.com/cgi-bin/token'
-        ret.appid = configer.get('App_info', 'id')
-        ret.secret = configer.get('App_info', 'secret')
-        return ret
 
     def _set_config(self):
         raise NotImplemented('Access.token._set_config not implemented')
