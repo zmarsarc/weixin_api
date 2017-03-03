50807295aa71fc1d8d3a132d24d59dd2b78d4c1e
zy <zeng.yu.1993@gmail.com>
2017-03-03 20:10:29 +0800

    + 为 Access.token 设计了单元测试例程。

--- test/TestAccess.py
+++ test/TestAccess.py
@@ -3,6 +3,8 @@
 import unittest
 from multiprocessing import Process
 from SampleServer import app
+from api import Access
+import time
 
 
 def start_server():
@@ -12,9 +14,27 @@ def start_server():
 
 class TestAccess(unittest.TestCase):
 
+    class config:
+        api = '127.0.0.1:12345/cgi-bin/token'
+        token = 'ACCESS_TOKEN_CACHE'
+        ttl = 7200
+        update = int(time.time())
+        id = 'zmarsarc'
+        secret = '12345'
+
     def setUp(self):
         self.server = Process(target=start_server)
         self.server.start()
+
+    def test_get_token_cache(self):
+        tk = Access.token(self.config)
+        self.assertEqual(self.config.token, tk.value)
+
+    def test_get_token_new(self):
+        config = self.config()
+        config.update = 0
+        tk = Access.token(config)
+        self.assertEqual('ACCESS_TOKEN', tk.value)
 
     def tearDown(self):
         self.server.terminate()
