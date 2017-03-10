fb6731500ad3939672bcf3e0a3dc8926692e8647
zy <zeng.yu.1993@gmail.com>
2017-03-10 19:46:02 +0800

    +  创建后台登录端口。

--- server_checker.py
+++ server_checker.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, url_for
 from hashlib import sha1
 from api import MessageType
 
@@ -21,6 +21,17 @@ def signature_checked(args):
     sorted_args = sorted([token, args['timestamp'], args['nonce']])
     local_signature = sha1(''.join(sorted_args)).hexdigest()
     return args['signature'] == local_signature
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' and request.form['password'] == '123456':
+            return 'success'
+        else:
+            return 'deny'
+    if request.method == 'GET':
+        return url_for('static', filename='admin_login.html')
 
 if __name__ == '__main__':
     app.run(port=8000)
