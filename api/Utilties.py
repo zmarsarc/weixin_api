2e2f94f89b634e17521a1f01a27034cb35d077a3
zy <zeng.yu.1993@gmail.com>
2017-03-13 01:04:39 +0800

    + 实现 Signleton，加锁，带 double check。

--- api/Utilties.py
+++ api/Utilties.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from hashlib import sha1
-
+from threading import Lock
 
 def signature_checked(args):
     sorted_args = sorted(['zmarsarc', args['timestamp'], args['nonce']])
@@ -13,10 +13,14 @@ class Signleton(type):
     def __new__(cls, *args, **kwargs):
         obj = super(Signleton, cls).__new__(cls, *args, **kwargs)
         setattr(obj, "_{0}__instance".format(cls.__name__), None)
+        setattr(obj, "_{0}__lock".format(cls.__name__), Lock())
         return obj
 
     def __call__(self, *args, **kwargs):
         if not self.__instance:
-            obj = self.__new__(self, *args, **kwargs)
-            self.__instance = obj
+            self.__lock.acquire()
+            if not self.__instance:
+                obj = self.__new__(self, *args, **kwargs)
+                self.__instance = obj
+            self.__lock.release()
         return self.__instance
