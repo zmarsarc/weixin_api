8d614c6fcdcf8525918a7e54aa18f4341c7f0db1
zy <zeng.yu.1993@gmail.com>
2017-03-13 01:06:33 +0800

    + 实现 FileConfig 单例，线程安全。

--- api/Config.py
+++ api/Config.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from ConfigParser import ConfigParser, NoOptionError
 from abc import abstractmethod, ABCMeta
-
-from .Logger import Logger
+from .Utilties import Signleton
 
 
 class OptionNotExistError(LookupError):
@@ -53,78 +51,16 @@ class AbstractConfig(object):
 
 class FileConfig(AbstractConfig):
 
-    def __init__(self, filename):
-        try:
-            super(FileConfig, self).__init__(filename)
-        except TypeError:
-            super(FileConfig, self).__init__()
+    __metaclass__ = Signleton
 
-        self.config_file = filename
-        self._configer = self._open_config_file()
-        self._section = self._define_section()
-        self._options = self._get_options()
-        self.__class__.__slots__ = [name for name in self._options.keys()]
-        self.__dict__.update(self._options)
-        self._limited = True
+    def __init__(self):
+        super(FileConfig, self).__init__()
 
-    def get(self, option, default=None):
-        return self.__dict__.get(option, default)
+    def add(self, option, value, group=None):
+        super(FileConfig, self).add(option, value, group)
 
-    def set(self, option, value):
-        if not hasattr(self, option):
-            raise NoOptionError(self._section, option)
-        self.add(option, value)
+    def get(self, option, group=None, default=None):
+        super(FileConfig, self).get(option, group, default)
 
-    def add(self, option, value):
-        self.__dict__[option] = value
-        self._configer.set(self._section, option, value)
-        self._write_config_file()
-
-    def _define_section(self):
-        raise NotImplementedError(__name__ + str(self.__class__) + 'not implemented')
-
-    def _define_slots(self):
-        raise NotImplementedError(__name__ + str(self.__class__) + ' _define_slots not implemented')
-
-    def _get_options(self):
-        return {name: value for (name, value) in self._configer.items(self._section)}
-
-    def _open_config_file(self):
-        try:
-            fp = open(self.config_file, 'r')  # 如果找不到配置文件，将会抛出 IOError
-        except IOError:
-            Logger.get_logger().warn('config file \"%s\" not exist', self.config_file)
-        fp.close()
-        configer = ConfigParser()
-        configer.read(self.config_file)
-        return configer
-
-    def _write_config_file(self):
-        with open(self.config_file, 'w') as fp:
-            Logger.get_logger().info("write config file \"%s\" in %s", self.config_file, self.__class__)
-            self._configer.write(fp)
-
-    def __setattr__(self, key, value):
-        if not hasattr(self, '_limited'):
-            self.__dict__['_limited'] = False
-        if self._limited and key not in self.__dict__.keys():
-            raise AttributeError('no such option ' + key)
-        self.__dict__[key] = value
-
-
-class app(FileConfig):
-
-    def _define_section(self):
-        return 'App'
-
-
-class server(FileConfig):
-
-    def _define_section(self):
-        return 'Server'
-
-
-class token(FileConfig):
-
-    def _define_section(self):
-        return 'Token'
+    def set(self, option, value, group=None):
+        super(FileConfig, self).set(option, value, group)
