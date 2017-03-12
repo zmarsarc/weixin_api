c6809f35424a6f1e45a8b283a472de78f0e1bb6f
zy <zeng.yu.1993@gmail.com>
2017-03-12 23:13:22 +0800

    + 实现 Signleton metaclass，不带线程锁。

--- api/Utilties.py
+++ api/Utilties.py
@@ -6,3 +6,17 @@ def signature_checked(args):
     sorted_args = sorted(['zmarsarc', args['timestamp'], args['nonce']])
     local_signature = sha1(''.join(sorted_args)).hexdigest()
     return args['signature'] == local_signature
+
+
+class Signleton(type):
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(Signleton, cls).__new__(cls, *args, **kwargs)
+        setattr(obj, "_{0}__instance".format(cls.__name__), None)
+        return obj
+
+    def __call__(self, *args, **kwargs):
+        if not self.__instance:
+            obj = self.__new__(self, *args, **kwargs)
+            self.__instance = obj
+        return self.__instance
