02ec7225330d94c648b021db966f46f39d73ec0f
zy <zeng.yu.1993@gmail.com>
2017-03-12 15:12:37 +0800

    + 为配置接口补充了文档。

--- api/Config.py
+++ api/Config.py
@@ -5,19 +5,49 @@ from abc import abstractmethod, ABCMeta
 from .Logger import Logger
 
 
+class OptionNotExistError(LookupError):
+    def __init__(self, *args, **kwargs):
+        super(OptionNotExistError, self).__init__(*args, **kwargs)
+        self.option = kwargs.get('option')
+
+
 class AbstractConfig(object):
+    """提供配置数据接口"""
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def get(self, option, default):
+    def get(self, option, group=None, default=None):
+        """
+        从配置集中读取配置项
+        :param option: str -> 指定配置项名称
+        :param group: （可选）指定配置项所在的组
+        :param default: （可选）未能成功读取配置时返回的值
+        :return: str
+        """
         pass
 
     @abstractmethod
-    def set(self, option, value):
+    def set(self, option, value, group=None):
+        """
+        设置配置集中指定项的值
+        :param option: str -> 指定配置项名称
+        :param value: str -> 需要写入的值
+        :param group: （可选）配置项所在的组
+        :return: int -> 成功写入的项目数
+        :raise: OptionNotExistError -> 指定的项目不存在
+        """
         pass
 
     @abstractmethod
-    def add(self, option, value):
+    def add(self, option, value, group=None):
+        """
+        在配置集中新建一个配置项
+        如果指定的项目已经存在，则覆盖
+        :param option: str -> 指定配置项目名称
+        :param value: str -> 指定要配置的值
+        :param group: （可选）str -> 配置项所在的配置组
+        :return: int -> 成功设置的配置数
+        """
         pass
 
 
