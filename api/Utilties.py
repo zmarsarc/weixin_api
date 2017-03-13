9251f00a48894d0a1e0c4de62431d1ee66ca0d6e
zy <zeng.yu.1993@gmail.com>
2017-03-13 18:12:45 +0800

    * 修正了 Signleton 中创建对象时没有对对象正确初始化的问题。

--- api/Utilties.py
+++ api/Utilties.py
@@ -2,6 +2,7 @@
 from hashlib import sha1
 from threading import Lock
 
+
 def signature_checked(args):
     sorted_args = sorted(['zmarsarc', args['timestamp'], args['nonce']])
     local_signature = sha1(''.join(sorted_args)).hexdigest()
@@ -9,6 +10,8 @@ def signature_checked(args):
 
 
 class Signleton(type):
+
+    # TODO：当与 ABCMeta 一起使用时，如何拦截 ABCMeta 的请求？
 
     def __new__(cls, *args, **kwargs):
         obj = super(Signleton, cls).__new__(cls, *args, **kwargs)
@@ -21,6 +24,7 @@ class Signleton(type):
             self.__lock.acquire()
             if not self.__instance:
                 obj = self.__new__(self, *args, **kwargs)
+                obj.__init__(*args, **kwargs)
                 self.__instance = obj
             self.__lock.release()
         return self.__instance
