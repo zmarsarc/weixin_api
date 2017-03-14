bee37e51e92f40976a4d3e9904b84f4f5670df35
zy <zeng.yu.1993@gmail.com>
2017-03-14 12:22:24 +0800

    * ACCESS 中已废弃的 config 用 FileConfig 替换，并且加入了 logger。

--- api/Access.py
+++ api/Access.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
-from ConfigParser import ConfigParser
 import requests
 import json
 import time
+from .Utilties import Signleton
+from .Logger import Logger
+import os
 
 
-def singleton(cls):
-    class wapped_class(cls):
-        _instance = None
-        
-        def __new__(cls, *args, **kwargs):
-            if wapped_class._instance is None:
-                wapped_class._instance = super(wapped_class, cls).__new__(cls, *args, **kwargs)
-            return wapped_class._instance
+class Token(object):
 
-    return wapped_class
-
-
-@singleton
-class token(object):
+    __metaclass__ = Signleton
 
     def __init__(self, config):
-        self._config = config  # 保存获取 token 所需的配置信息，并且将 token 回写。
+        super(Token, self).__init__()
 
-        self.__value = config.token
-        self.update = config.update
-        self.ttl = config.ttl
-
+        self.logger = Logger.get_logger()
+        self.config = config  # 保存获取 token 所需的配置信息，并且将 token 回写。
+        with open(os.path.join(os.getcwd(), 'api\\data\\weixin.api')) as fp:
+            self.api = json.load(fp)
+            self.logger.info("module {0} read api file {1}".format(self.__class__, fp.name))
+        self.token = self.config.get('token')
+        self.__value = self.token['value']
+        self.update = int(self.token['update'])
+        self.ttl = int(self.token['ttl'])
         if time.time() - self.update >= self.ttl:
             self.refresh()
 
@@ -39,17 +35,17 @@ class token(object):
     def refresh(self):
         self.__value, self.ttl = self._get_token()
         self.update = int(time.time())
+        self.logger.info("refresh token, ttl {0}".format(self.ttl))
         self._set_config()
 
     def _get_token(self):
         params = {'grant_type': 'client_credential',
-                  'appid': self._config.id,
-                  'secret': self._config.secret}
-        response = requests.get(self._config.api, params=params)
+                  'appid': self.config.get('appid'),
+                  'secret': self.config.get('secret')}
+        response = requests.get(self.api["base"] + self.api['token']["api"], params=params)
         response = json.loads(response.content)
         return (response['access_token'], response['expires_in'])
 
     def _set_config(self):
-        self._config.token = self.__value
-        self._config.ttl = self.ttl
-        self._config.update = self.update
+        self.config.set('token', {"value": self.__value, "ttl": self.ttl, "update": self.update})
+        self.logger.info("write token to config")
