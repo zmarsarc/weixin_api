3bbfee18e1cb02e3451d59e114a0e9299514b983
zy <zeng.yu.1993@gmail.com>
2017-04-07 20:24:28 +0800

    fix bugs when check password

--- server_checker.py
+++ server_checker.py
@@ -54,7 +54,7 @@ def process_request(name):
 def password_check(username, password, db):
     result = db.execute("SELECT password FROM password WHERE username IS ?;", (username,))
     fetch = result.fetchone()
-    return fetch == password
+    return fetch[0] == password
 
 
 if __name__ == '__main__':
